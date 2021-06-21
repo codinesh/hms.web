41ad0db4e35ab3b812cd53757b9d081b0c896925
Dinesh <7272358+codinesh@users.noreply.github.com>
2021-06-21 17:19:23 +0530

    test

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -27,38 +27,7 @@ const Layout: React.FC = (props) => {
             <MenuAlt2Icon className='w-6 h-6' aria-hidden='true' />
           </button>
           <div className='flex justify-between flex-1 px-4'>
-            <div className='flex flex-1'>
-              <form
-                className='flex w-full md:ml-0'
-                method='GET'
-                onSubmit={(e) => {
-                  globalDispatch({
-                    type: GlobalStateAction.SetSearchQuery,
-                    searchQuery: query,
-                  });
-                  e.preventDefault();
-                }}>
-                <label htmlFor='search_field' className='sr-only'>
-                  Search
-                </label>
-                <div className='relative w-full text-gray-400 focus-within:text-gray-600'>
-                  <div className='absolute inset-y-0 left-0 flex items-center pointer-events-none'>
-                    <SearchIcon className='w-5 h-5' aria-hidden='true' />
-                  </div>
-                  <input
-                    onChange={(e) => {
-                      setQuery(e.target.value);
-                    }}
-                    value={query}
-                    id='search_field'
-                    className='block w-full h-full py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 border-transparent focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm'
-                    placeholder='Search'
-                    type='search'
-                    name='search'
-                  />
-                </div>
-              </form>
-            </div>
+            <div className='flex flex-1'></div>
             <div className='flex items-center ml-4 md:ml-6'>
               <button className='p-1 text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                 <span className='sr-only'>View notifications</span>
