3770e4bc74e36645f98aeffef32b750c3a6ec6f0
Dinesh <7272358+codinesh@users.noreply.github.com>
2021-07-17 15:47:28 +0530

    fix

--- pages/lab/reports/[id].tsx
+++ pages/lab/reports/[id].tsx
@@ -179,7 +179,7 @@ const LabInvoiceDetail: React.FC<{ labInvoice: LabInvoice }> = (props) => {
                         aria-hidden='true'
                       />
                       <span className='ml-2 flex-1 w-0 truncate'>
-                        Lab Report 1
+                        Lab Report
                       </span>
                     </div>
                     <div className='ml-4 flex-shrink-0'>
@@ -217,7 +217,7 @@ const LabInvoiceDetail: React.FC<{ labInvoice: LabInvoice }> = (props) => {
       <div className='bg-white shadow overflow-hidden sm:rounded-lg'>
         <div className='px-4 py-5 sm:px-6'>
           <h3 className='text-lg leading-6 font-medium text-gray-900'>
-            Lab tests {JSON.stringify(testResultsState)}
+            Lab tests
           </h3>
           <p className='mt-1 max-w-2xl text-sm text-gray-500'>test reports.</p>
         </div>
@@ -226,11 +226,11 @@ const LabInvoiceDetail: React.FC<{ labInvoice: LabInvoice }> = (props) => {
             {labInvoice.invoiceDetail.map((test) => {
               return (
                 <div className='flex items-center pr-4'>
-                  <div className='flex-grow py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <div className='flex-grow py-4  flex gap-1 px-6'>
                     <dt className='text-sm font-medium text-gray-500'>
                       {test.testName}
                     </dt>
-                    <dd className='mt-1 divide-y  text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                    <dd className='mt-1 divide-y flex flex-col text-sm text-gray-900 sm:mt-0 '>
                       {test.labTestRecord.labTestResult.map((x) => {
                         let value: any = testResultsState.get(
                           x.labTestRecordId.toString()
@@ -242,13 +242,15 @@ const LabInvoiceDetail: React.FC<{ labInvoice: LabInvoice }> = (props) => {
 
                         return (
                           <dl className='sm:divide-y sm:divide-gray-200'>
-                            <div className=' py-4 bg-red-100 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                              <dt className='text-sm font-medium text-gray-500'>
-                                {x.referenceRange}
+                            <div className='flex gap-4 py-4 sm:py-5 px-6'>
+                              <dt className=' flex flex-col w-full  text-sm font-medium text-gray-500'>
+                                <span>{x.test}</span>
+                                <span>{x.referenceRange}</span>
                               </dt>
-                              <dd className='bg-red-100 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                              <dd className='mt-1 text-sm  text-gray-900 sm:mt-0'>
                                 <input
                                   type='text'
+                                  className=' shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md'
                                   value={testResult}
                                   onChange={(e) => {
                                     var res: any = testResultsState.get(
