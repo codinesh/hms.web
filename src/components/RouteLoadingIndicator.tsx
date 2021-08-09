ef79e4fd22608554f04b2c38b7b5b3b3d8cd222e
Dinesh <7272358+codinesh@users.noreply.github.com>
2021-08-09 13:04:01 +0530

    Removed NProgress references

--- src/components/RouteLoadingIndicator.tsx
+++ src/components/RouteLoadingIndicator.tsx
@@ -1,16 +1,13 @@
 import Router from 'next/router'
 import React, { useEffect, useState } from 'react'
 import LoadingIndicator from './LoadingIndicator'
-import NProgress from 'nprogress'
 
 const RouteLoadingIndicator = () => {
   function routeChangeStart(arg0: string, routeChangeStart: any) {
     setRouteLoading(true)
-    // NProgress.start()
   }
 
   function routeChangeEnd(arg0: string, routeChangeEnd: any) {
-    // NProgress.done()
     setRouteLoading(false)
   }
 
--- src/types/ILoadingState.ts
+++ src/types/ILoadingState.ts
@@ -1,6 +1,6 @@
-import Doctor from '../models/Doctor';
-import Patient from '../models/Patient';
-import IUser from "./IUser"
+import Doctor from '../models/Doctor'
+import Patient from '../models/Patient'
+import IUser from './IUser'
 
 export interface ILoadingState {
   inProgress: boolean
